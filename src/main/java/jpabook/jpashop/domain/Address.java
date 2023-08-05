a3ae8eb1b5c5870d94988dec8a08efff8f158649
박정현 <ros28081@naver.com>
2023-08-05 16:29:30 +0900

    [FEAT] Address 객체에 생성자 추가

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -10,4 +10,14 @@ public class Address {
     private String city;
     private String street;
     private String zipecode;
+
+    // Embedded type 이므로 protected 설정
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipecode) {
+        this.city = city;
+        this.street = street;
+        this.zipecode = zipecode;
+    }
 }
