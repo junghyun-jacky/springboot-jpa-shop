e0ff055f4e778d7474e6b8cd9cba6657a60a4d7c
박정현 <ros28081@naver.com>
2023-08-13 22:02:50 +0900

    [FEAT] Member 컨트롤러 구현

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -1,10 +1,15 @@
 package jpabook.jpashop.controller;
 
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -12,9 +17,28 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    // form 화면 열기
     @GetMapping("/members/new")
     public String createForm(Model model) {
         model.addAttribute("memberForm",  new MemberForm());
         return "members/createMemberForm";
+    }
+
+    // 회원 가입 데이터 등록
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {    // 회원가입 항목이 Member 엔티티와 일치하지 않기 때문에 따로 MemberForm 사용
+
+        if(result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
     }
 }
